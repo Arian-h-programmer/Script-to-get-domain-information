ce6ac53d118a4deb858c0b26d4d4524e8b8fe44c
Arian-programmer369 <83920334+Arian-programmer369@users.noreply.github.com>
2021-05-10 11:00:34 +0430

    Add files via upload

---
+++ code.py
@@ -0,0 +1,11 @@
+# Arian.programmer
+
+# In cmd or terminal type : pip install python-whois
+
+import whois 
+
+site = input('Enter your desired site: ') 
+
+a = whois.whois(site) 
+
+print(a) 
---
+++ programm.py
@@ -0,0 +1,9 @@
+# Arian.programmer
+
+# In cmd or terminal type : pip install python-whois
+
+import whois 
+
+a = whois.whois('google.com')  
+
+print(a) 
